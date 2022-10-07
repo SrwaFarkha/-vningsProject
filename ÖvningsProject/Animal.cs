c129ba10c86a319ba49322219881b48bbedf4ecb
srwa.farkha@hotmail.com <srwa.farkha@hotmail.com>
2022-10-07 09:29:36 +0200

    Add project files.

---
+++ ÖvningsProject/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace ÖvningsProject;
+
+public class Animal
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Legs { get; set; }
+    public string Size { get; set; }
+
+   
+}
---
+++ ÖvningsProject/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using ÖvningsProject;
+
+
+
+var animals = new List<Animal>() {
+    new Animal(){Name="Ko", Age= 4 , Legs= 4 , Size= "big"},
+    new Animal(){ Name="Tupp", Age= 2 , Legs= 2 , Size= "Smal"},
+    new Animal(){ Name="Häst", Age= 14 , Legs= 4 , Size= "big"},
+    new Animal(){ Name="Katt", Age= 8 , Legs= 4 , Size= "Smal"}
+};
+
+Console.WriteLine($"Here is the list of the animals: ");
+
+foreach (var result in animals)
+{
+    Console.WriteLine($" Name: {result.Name}, Age: {result.Age}, Number  of legs: {result.Legs}, Size: {result.Size}");
+}
+
+Console.WriteLine();
+Console.WriteLine("Would you like to add another animal?");
+string answear = Console.ReadLine().ToLower();
+if (answear == "yes")
+{
+    Console.WriteLine("Write: Name, Age, Legs and size. klick tap after each one.");
+    string _name = Console.ReadLine();
+    string _age = Console.ReadLine();
+    string _legs = Console.ReadLine();
+    string _size = Console.ReadLine();
+    Console.WriteLine();
+    Console.WriteLine($"Your animal is: Name: {_name}, Age: {_age}, Number os legs: {_legs} and size: {_size}");
+}
+else
+{
+    Console.WriteLine("Okej, have a good day!");
+}
+
